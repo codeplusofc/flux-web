825502255fe60554cc8e9c05b8bfb5a90d912a2e
GuiRuchell <ruchellguilherme100@gmail.com>
2025-08-14 15:12:59 -0300

    feat:initial version

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flux.app',
+  appName: 'fluxapp',
+  webDir: 'www'
+};
+
+export default config;
